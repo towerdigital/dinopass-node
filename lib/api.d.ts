b70ecbfd2e4948c298e5f9cba3d03e7417022f5f
Phillip Henslee <ph2@ph2.us>
2019-08-31 10:05:25 -0500

    add comments

--- lib/api.d.ts
+++ lib/api.d.ts
@@ -1,2 +1,11 @@
-declare const fetchPassword: (type: string) => Promise<import("axios").AxiosResponse<any>>;
-export { fetchPassword };
+/**
+ * Copyright (c) Tower Digital LLC and Phillip Henslee. All Rights Reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+declare const fetchPassword: (
+    type: string
+) => Promise<import('axios').AxiosResponse<any>>
+export { fetchPassword }
--- lib/dinopass.d.ts
+++ lib/dinopass.d.ts
@@ -1,4 +1,26 @@
-declare const getSimplePassword: (n?: number) => Promise<any>;
-declare const getStrongPassword: (n?: number) => Promise<any>;
-declare const getOraclifiedPassword: (n?: number) => Promise<any>;
-export { getSimplePassword, getStrongPassword, getOraclifiedPassword };
+/**
+ * Copyright (c) Tower Digital LLC and Phillip Henslee. All Rights Reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+/**
+ * @description Returns one or more simple passwords
+ * @param n Number of passwords.
+ * @returns Promise<any>
+ */
+declare const getSimplePassword: (n?: number) => Promise<any>
+/**
+ * @description Returns one or more strong passwords
+ * @param n Number of passwords.
+ * @returns Promise<any>
+ */
+declare const getStrongPassword: (n?: number) => Promise<any>
+/**
+ * @description Returns one or more oraclified passwords
+ * @param n Number of passwords.
+ * @returns Promise<any>
+ */
+declare const getOraclifiedPassword: (n?: number) => Promise<any>
+export { getSimplePassword, getStrongPassword, getOraclifiedPassword }
