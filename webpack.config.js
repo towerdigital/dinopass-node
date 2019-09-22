ca8870d156bba8a4baf14cbdc14612268abfa7a2
Phillip Henslee <ph2@ph2.us>
2019-09-22 11:02:18 -0500

    add banner

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,10 @@
 const path = require('path');
 const webpack = require('webpack');
+const PACKAGE = require('./package.json');
+
+const banner =
+  `${PACKAGE.name} - ${PACKAGE.version} | ` +
+  `(c) ${PACKAGE.author} | ${PACKAGE.license} | ${PACKAGE.homepage}`;
 
 const PATHS = {
   entryPoint: path.resolve(__dirname, 'src/index.ts'),
@@ -7,18 +12,10 @@ const PATHS = {
 };
 
 const config = {
-  // These are the entry point of our library. We tell webpack to use
-  // the name we assign later, when creating the bundle. We also use
-  // the name to filter the second entry point for applying code
-  // minification via UglifyJS
   entry: {
     'dinopass.min': [PATHS.entryPoint]
   },
   mode: 'production',
-  // The output defines how and where we want the bundles. The special
-  // value `[name]` in `filename` tell Webpack to use the name we defined above.
-  // We target a UMD and name it MyLib. When including the bundle in the browser
-  // it will be accessible at `window.MyLib`
   output: {
     path: PATHS.bundles,
     filename: '[name].js',
@@ -26,18 +23,19 @@ const config = {
     library: 'DinoPass',
     umdNamedDefine: true
   },
-  // Add resolve for `tsx` and `ts` files, otherwise Webpack would
-  // only look for common JavaScript file extension (.js)
   resolve: {
     extensions: ['.ts', '.tsx', '.js']
   },
-  // Activate source maps for the bundles in order to preserve the original
-  // source when the user debugs the application
   devtool: 'source-map',
   externals: {
     axios: 'axios'
   },
-  // Add the loader for .ts files.
+  plugins: [
+    new webpack.BannerPlugin({
+      banner,
+      entryOnly: true
+    })
+  ],
   module: {
     rules: [
       {
